212a574a9f671a9d48969a6b9a11c03775c167dc
Santiago Ariel <santiagoarielv98@gmail.com>
2025-04-10 01:33:50 -0300

    feat: refactor recipe form to enhance user experience with dynamic input fields and dietary restrictions

--- src/app/components/recipe-form/recipe-form.component.ts
+++ src/app/components/recipe-form/recipe-form.component.ts
@@ -58,124 +58,72 @@ import { MatIconModule } from '@angular/material/icon';
 export class RecipeFormComponent {
   private fb = inject(FormBuilder);
   addressForm = this.fb.group({
-    company: null,
-    firstName: [null, Validators.required],
-    lastName: [null, Validators.required],
-    address: [null, Validators.required],
-    address2: null,
-    city: [null, Validators.required],
-    state: [null, Validators.required],
-    postalCode: [
-      null,
-      Validators.compose([
-        Validators.required,
-        Validators.minLength(5),
-        Validators.maxLength(5),
-      ]),
-    ],
-    shipping: ['free', Validators.required],
+    recipeInput: [null, Validators.required],
+    recipeType: ['idea', Validators.required],
   });
-  readonly reactiveKeywords = signal([
-    'angular',
-    'how-to',
-    'tutorial',
-    'accessibility',
+
+  readonly dietaryRestrictions = signal([
+    'Sin TACC',
+    'Vegano',
+    'Vegetariano',
+    'Sin lactosa',
+    'Sin frutos secos',
   ]);
-  readonly formControl = new FormControl(['angular']);
+
+  readonly formControl = new FormControl(['Sin TACC']);
 
   announcer = inject(LiveAnnouncer);
 
-  removeReactiveKeyword(keyword: string) {
-    this.reactiveKeywords.update((keywords) => {
-      const index = keywords.indexOf(keyword);
+  removeDietaryRestriction(restriction: string) {
+    this.dietaryRestrictions.update((restrictions) => {
+      const index = restrictions.indexOf(restriction);
       if (index < 0) {
-        return keywords;
+        return restrictions;
       }
 
-      keywords.splice(index, 1);
-      this.announcer.announce(`removed ${keyword} from reactive form`);
-      return [...keywords];
+      restrictions.splice(index, 1);
+      this.announcer.announce(`Se eliminó ${restriction} de las restricciones`);
+      return [...restrictions];
     });
   }
 
-  addReactiveKeyword(event: MatChipInputEvent): void {
+  addDietaryRestriction(event: MatChipInputEvent): void {
     const value = (event.value || '').trim();
 
-    // Add our keyword
+    // Add our restriction
     if (value) {
-      this.reactiveKeywords.update((keywords) => [...keywords, value]);
-      this.announcer.announce(`added ${value} to reactive form`);
+      this.dietaryRestrictions.update((restrictions) => [
+        ...restrictions,
+        value,
+      ]);
+      this.announcer.announce(`Se agregó ${value} a las restricciones`);
     }
 
     // Clear the input value
     event.chipInput!.clear();
   }
 
-  hasUnitNumber = false;
-
-  states = [
-    { name: 'Alabama', abbreviation: 'AL' },
-    { name: 'Alaska', abbreviation: 'AK' },
-    { name: 'American Samoa', abbreviation: 'AS' },
-    { name: 'Arizona', abbreviation: 'AZ' },
-    { name: 'Arkansas', abbreviation: 'AR' },
-    { name: 'California', abbreviation: 'CA' },
-    { name: 'Colorado', abbreviation: 'CO' },
-    { name: 'Connecticut', abbreviation: 'CT' },
-    { name: 'Delaware', abbreviation: 'DE' },
-    { name: 'District Of Columbia', abbreviation: 'DC' },
-    { name: 'Federated States Of Micronesia', abbreviation: 'FM' },
-    { name: 'Florida', abbreviation: 'FL' },
-    { name: 'Georgia', abbreviation: 'GA' },
-    { name: 'Guam', abbreviation: 'GU' },
-    { name: 'Hawaii', abbreviation: 'HI' },
-    { name: 'Idaho', abbreviation: 'ID' },
-    { name: 'Illinois', abbreviation: 'IL' },
-    { name: 'Indiana', abbreviation: 'IN' },
-    { name: 'Iowa', abbreviation: 'IA' },
-    { name: 'Kansas', abbreviation: 'KS' },
-    { name: 'Kentucky', abbreviation: 'KY' },
-    { name: 'Louisiana', abbreviation: 'LA' },
-    { name: 'Maine', abbreviation: 'ME' },
-    { name: 'Marshall Islands', abbreviation: 'MH' },
-    { name: 'Maryland', abbreviation: 'MD' },
-    { name: 'Massachusetts', abbreviation: 'MA' },
-    { name: 'Michigan', abbreviation: 'MI' },
-    { name: 'Minnesota', abbreviation: 'MN' },
-    { name: 'Mississippi', abbreviation: 'MS' },
-    { name: 'Missouri', abbreviation: 'MO' },
-    { name: 'Montana', abbreviation: 'MT' },
-    { name: 'Nebraska', abbreviation: 'NE' },
-    { name: 'Nevada', abbreviation: 'NV' },
-    { name: 'New Hampshire', abbreviation: 'NH' },
-    { name: 'New Jersey', abbreviation: 'NJ' },
-    { name: 'New Mexico', abbreviation: 'NM' },
-    { name: 'New York', abbreviation: 'NY' },
-    { name: 'North Carolina', abbreviation: 'NC' },
-    { name: 'North Dakota', abbreviation: 'ND' },
-    { name: 'Northern Mariana Islands', abbreviation: 'MP' },
-    { name: 'Ohio', abbreviation: 'OH' },
-    { name: 'Oklahoma', abbreviation: 'OK' },
-    { name: 'Oregon', abbreviation: 'OR' },
-    { name: 'Palau', abbreviation: 'PW' },
-    { name: 'Pennsylvania', abbreviation: 'PA' },
-    { name: 'Puerto Rico', abbreviation: 'PR' },
-    { name: 'Rhode Island', abbreviation: 'RI' },
-    { name: 'South Carolina', abbreviation: 'SC' },
-    { name: 'South Dakota', abbreviation: 'SD' },
-    { name: 'Tennessee', abbreviation: 'TN' },
-    { name: 'Texas', abbreviation: 'TX' },
-    { name: 'Utah', abbreviation: 'UT' },
-    { name: 'Vermont', abbreviation: 'VT' },
-    { name: 'Virgin Islands', abbreviation: 'VI' },
-    { name: 'Virginia', abbreviation: 'VA' },
-    { name: 'Washington', abbreviation: 'WA' },
-    { name: 'West Virginia', abbreviation: 'WV' },
-    { name: 'Wisconsin', abbreviation: 'WI' },
-    { name: 'Wyoming', abbreviation: 'WY' },
-  ];
+  resetForm(): void {
+    this.addressForm.reset({
+      recipeInput: null,
+      recipeType: 'idea',
+    });
+    this.dietaryRestrictions.set([
+      'Sin TACC',
+      'Vegano',
+      'Vegetariano',
+      'Sin lactosa',
+      'Sin frutos secos',
+    ]);
+    this.formControl.setValue(['Sin TACC']);
+    this.announcer.announce('Formulario reiniciado');
+  }
 
   onSubmit(): void {
-    alert('Thanks!');
+    if (this.addressForm.valid) {
+      alert('¡Receta generada!');
+    } else {
+      this.announcer.announce('Por favor completa los campos requeridos');
+    }
   }
 }
